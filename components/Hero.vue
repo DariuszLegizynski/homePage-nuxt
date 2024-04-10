e9575d6e263d4605d65bb7f144630c461e99c578
Dariusz Legizynski <DLegizynski@anexia-it.com>
2024-04-10 22:55:49 +0200

    first commit

---
+++ components/Hero.vue
@@ -0,0 +1,30 @@
+<template>
+	<article class="relative">
+		<div ref="container">
+			<div class="styles.inner">
+				<div className="styles.title">
+					<h1 ref="textRef">
+						I&apos;m Dariusz Legizynski
+						<span className="`$'styles.icon'`">
+							<span className="styles.reveal"></span>
+							<img
+								src="/"
+								alt="Something"
+								width="640"
+								height="300"
+								loading="lazy"
+								class="styles.heroImg"
+							/>
+						</span>
+						<br />An Innovative Frontend Developer.
+					</h1>
+					<p ref="descRef">
+						I develop accessible, responsive, interactive, and animated websites
+						with a strong focus on performance.
+					</p>
+				</div>
+			</div>
+		</div>
+	</article>
+</template>
+
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+	<article>
+		<p>I Am Layout Header</p>
+		<slot></slot>
+		<p>I am Layout Footer</p>
+	</article>
+</template>
+
